16d6b443473c1d9f07e077deac65cee26784b219
Jenna Miller <jennamiller_92@hotmail.com>
2014-12-15 14:34:09 -0600

    Initial Commit

---
+++ Blocstogram/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Blocstogram
+//
+//  Created by Jenna Miller on 12/15/14.
+//  Copyright (c) 2014 Bloc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Blocstogram/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Blocstogram
+//
+//  Created by Jenna Miller on 12/15/14.
+//  Copyright (c) 2014 Bloc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
