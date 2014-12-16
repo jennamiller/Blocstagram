fc4bb0d8cff807e97dba0e2cdea990e60e667860
Jenna Miller <jennamiller_92@hotmail.com>
2014-12-15 18:55:10 -0600

    table view controller implemented

---
+++ Blocstogram/BLCImagesTableViewController.h
@@ -0,0 +1,14 @@
+//
+//  BLCImagesTableViewController.h
+//  Blocstogram
+//
+//  Created by Jenna Miller on 12/15/14.
+//  Copyright (c) 2014 Bloc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BLCImagesTableViewController : UITableViewController
+@property (nonatomic, strong) NSMutableArray *images;
+
+@end
